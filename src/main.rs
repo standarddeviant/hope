9e8aa687d01a8a8f1f072d54b4d38aa976de1724
standarddeviant <dave.crist@gmail.com>
2026-03-11 08:27:00 -0400

    async inbox works, no bt yet

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use eframe::egui;
+use egui::CentralPanel;
+use egui_inbox::UiInbox;
+
+use tokio::runtime::Runtime;
+
+pub fn main() -> eframe::Result<()> {
+    let inbox = UiInbox::new();
+    let mut state = None;
+
+    eframe::run_simple_native(
+        "DnD Simple Example",
+        Default::default(),
+        move |ctx, _frame| {
+            CentralPanel::default().show(ctx, |ui| {
+                // `read` will return an iterator over all pending messages
+                if let Some(last) = inbox.read(ui).last() {
+                    state = last;
+                }
+                // There also is a `replace` method that you can use as a shorthand for the above:
+                // inbox.replace(ui, &mut state);
+
+                ui.label(format!("State: {:?}", state));
+                if ui.button("Async Task").clicked() {
+                    state = Some("Waiting for async task to complete".to_string());
+                    let tx = inbox.sender();
+                    std::thread::spawn(move || {
+                        let rt = Runtime::new().expect("Failed to create runtime");
+                        rt.block_on(async {
+                            std::thread::sleep(std::time::Duration::from_secs(1));
+                            // Send will return an error if the receiver has been dropped
+                            // but unless you have a long running task that will send multiple messages
+                            // you can just ignore the error
+                            tx.send(Some("Hello from another thread!".to_string())).ok();
+                        });
+                    });
+                }
+            });
+        },
+    )
+}
